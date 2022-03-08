5306fda1dbb06b1ce8b62075c60318a673339fbe
Jan Dalheimer <jan@dalheimer.de>
2022-03-08 22:28:32 +0100

    Slight rework of language selection

--- view/home.js
+++ view/home.js
@@ -1,4 +1,5 @@
 import { findOneOrFail, setMainView } from "./helpers.js";
+import { setCurrentLocale } from "../controller/languages.js";
 
 findOneOrFail("#button-is-customer").addEventListener("click", () =>
   setMainView("customer-home")
@@ -8,17 +9,6 @@ findOneOrFail("#button-is-bartender").addEventListener("click", () =>
   setMainView("bartender")
 );
 
-window.changeLanguage = function(){
-    const swe = document.getElementById('swedish-button');
-    const eng = document.getElementById('english-button');
-    swe.addEventListener("click", () =>{
-        swe.src = "/images/sweden-activated.png";
-        eng.src = "/images/united-kingdom.png";
-        setLanguage('sv');
-    })
-    eng.addEventListener("click", () =>{
-        swe.src = "/images/sweden.png";
-        eng.src = "/images/united-kingdom-activated.png";
-        setLanguage('en');
-    });
-}
+window.changeLanguage = function (changeTo) {
+  setCurrentLocale(changeTo);
+};
