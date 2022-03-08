a470f1df4f32c441e0379051a295a90b13c678a2
Hana Nabilah Fatinah <56430915+raihanahnabilah@users.noreply.github.com>
2022-03-08 13:28:54 +0100

    Menu Drag and Drop

--- view/menu.js
+++ view/menu.js
@@ -1,14 +1,10 @@
-//const products = {
-//get element by id from
-
-//for products in
-
-//element. create child
-//}
-
 import { beverages } from "../model/data/beverages.js";
 import {findOneOrFail, setMainView} from "./helpers.js";
 
+/***
+ * Set the language in the menu
+ * This should be moved to controller, I believe!
+ */
 window.menuLanguage = function(){
     const menulang = document.getElementsByClassName('menu-language-li');
     const langimg = document.getElementsByClassName('img-language');
@@ -23,57 +19,107 @@ window.menuLanguage = function(){
     });
 }
 
-function clickHandler(element) {
-  //element.properties/id
-  var parent = element.parentElement.parentElement
-  for (let i = 0; i < parent.length; i++) {
-    parent.children[i].children[0].classList.remove("selected")
-  }
+/**
+ * Itemcount to test the number of items added
+ * Will be removed when we're triggering creation of OrderBill instead!
+ * @type {number}
+ */
+var itemCount = 0;
 
-  if (!element.classList.contains("selected")) {
-    element.classList.toggle("selected");
-  }
-}
-window.clickHandler = clickHandler;
-
-function submitHandler(element) {
-  // for (let i = 0; i < element.children.length; i++) {
-  //   element.children[i].classList.remove("selected")
-  // }
-  getElementById
+/***
+ * Function to add a class for drag and drop
+ */
+function addDnDClass(){
+    var element = document.getElementById("cart-title");
+    element.classList.add("cart-dnd");
 }
 
-function logoutHandler(element) {
-  // for (let i = 0; i < element.children.length; i++) {
-  //   element.children[i].classList.remove("selected")
-  // }
+/***
+ * Function to remove a class for drag and drop
+ */
+function removeDnDClass(){
+    var element = document.getElementById("cart-title");
+    element.classList.remove("cart-dnd");
 }
-window.logoutHandler = logoutHandler;
 
-function allowDrop(ev) {
-  ev.preventDefault();
+/***
+ * AllowDrop function
+ */
+window.allowDrop = (ev) => {
+    ev.preventDefault();
 }
-window.allowDrop = allowDrop;
 
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
+
+/***
+ * Drag function
+ */
+window.drag = (ev) => {
+    ev.dataTransfer.setData("Item", ev.target.id);
 }
-window.drag = drag;
 
-function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
+/***
+ * Drop function
+ */
+window.drop = (ev) => {
+    itemCount = itemCount + 1;
+    event.preventDefault();
+    var data= event.dataTransfer.getData("Item");
+    document.getElementById('cart-title').innerHTML = ("Cart ("+itemCount+")");
 }
-window.drop = drop;
 
+/***
+ * DnD effect -- dragstart
+ */
+document.addEventListener("dragstart", function( event ) {
+    // make it half transparent
+    event.target.style.opacity = .5;
+    addDnDClass();
+}, false);
+
+/***
+ * DnD effect -- dragend
+ */
+document.addEventListener("dragend", function( event ) {
+    // reset the transparency
+    event.target.style.opacity = "1";
+    removeDnDClass();
+}, false);
+
+/***
+ * DnD effect -- dragenter
+ */
+document.addEventListener("dragenter", function( event ) {
+    // make it half transparent
+    if (event.target.id == "cart-title"){
+        removeDnDClass();
+    }
+}, false);
+
+/***
+ * DnD effect -- dragleave
+ */
+document.addEventListener("dragleave", function( event ) {
+    // make it half transparent
+    if (event.target.id == "cart-title"){
+        addDnDClass();
+    }
+}, false);
+
+/**
+ * Container to show the menu list
+ * @type {HTMLElement}
+ */
 const menu = document.getElementById('insertMenu');
 
+/***
+ * Show all items in the menu (by default)
+ * Havent been updated yet for selections of "All Items", "Beer", "Wine", etc
+ */
 for (var i = 0; i < beverages.length; i++){
   let name = beverages[i]['namn'];
   let price = beverages[i]['prisinklmoms'];
   let html = `
-    <div class="card">
+    <div class="card" draggable="true" ondragstart="drag(event)">
     <a href="#" id="to-product-page">
           <div class="card-container">
             <h4 class="item-title">${name}
