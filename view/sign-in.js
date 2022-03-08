369fe85148e5a2b0532a03550ce6fcd0f9b95fbe
Jan Dalheimer <jan@dalheimer.de>
2022-03-08 22:28:46 +0100

    Hook up sign-in page

--- view/sign-in.js
+++ view/sign-in.js
@@ -1,1 +1,14 @@
-export {};
+import { findOneOrFail, setMainView } from "./helpers.js";
+import signInController from "../controller/sign_in_controller.js";
+
+findOneOrFail("#sign-in-button").addEventListener("click", () => {
+  const elUsername = document.getElementById("uname");
+  const elPassword = document.getElementById("password");
+  if (signInController.trySignIn(elUsername.value, elPassword.value)) {
+    setMainView("menu");
+    elUsername.value = "";
+    elPassword.value = "";
+  } else {
+    document.getElementById("signin-error").classList.remove("display-none");
+  }
+});
